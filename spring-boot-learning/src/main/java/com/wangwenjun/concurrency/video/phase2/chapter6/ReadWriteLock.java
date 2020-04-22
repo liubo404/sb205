82a92fcb3232e6d6b10ba252cd887554fbfc1e80
liubo <liubo404@gmail.com>
2020-04-22 16:28:29 +0800

    rw lock

--- spring-boot-learning/src/main/java/com/wangwenjun/concurrency/video/phase2/chapter6/ReadWriteLock.java
+++ spring-boot-learning/src/main/java/com/wangwenjun/concurrency/video/phase2/chapter6/ReadWriteLock.java
@@ -10,9 +10,52 @@ public class ReadWriteLock {
 	private int writingWriters = 0;
 	private int waitingWriters = 0;
 
+	//---------------------------------------------------------------------
+	// 1.read lock
+	//---------------------------------------------------------------------
 
-	public synchronized void readLock(){
+
+	public synchronized void readLock() throws InterruptedException {
 		this.waitingReaders++;
+		try {
+			while (waitingWriters > 0) {
+				this.wait();
+			}
+			this.readingReaders++;
+		} finally {
+			this.waitingReaders--;
+		}
+	}
 
+	public synchronized void readUnlock() {
+		this.readingReaders--;
+		this.notifyAll();
+	}
+
+
+	//---------------------------------------------------------------------
+	// 2.write lock
+	//---------------------------------------------------------------------
+
+
+	public synchronized void writeLock() throws InterruptedException {
+		this.waitingWriters++;
+
+		try {
+
+			while (this.readingReaders > 0 || this.writingWriters > 0) {
+				this.wait();
+			}
+
+			this.writingWriters++;
+
+		} finally {
+			this.waitingWriters--;
+		}
+	}
+
+	public synchronized void writeUnlock() {
+		this.writingWriters--;
+		this.notifyAll();
 	}
 }
